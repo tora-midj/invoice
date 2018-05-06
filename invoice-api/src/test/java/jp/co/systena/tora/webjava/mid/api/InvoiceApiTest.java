841d7fa33742339cfeba509abbacd266327e0fac
reo <devloprk555@gmail.com>
2018-05-06 16:36:15 +0900

    追加実装

--- invoice-api/src/test/java/jp/co/systena/tora/webjava/mid/api/InvoiceApiTest.java
+++ invoice-api/src/test/java/jp/co/systena/tora/webjava/mid/api/InvoiceApiTest.java
@@ -2,7 +2,6 @@ package jp.co.systena.tora.webjava.mid.api;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -38,7 +37,6 @@ import lombok.extern.slf4j.Slf4j;
 @DbUnitConfiguration(dataSetLoader = CsvDataSetLoader.class // ここでCSVでデータ読み込むReplacementDataSetLoaderのクラスを指定
 )
 @Slf4j
-@Ignore
 public class InvoiceApiTest extends InvoiceApiTestBase {
 
     @Test
